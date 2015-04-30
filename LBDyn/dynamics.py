790bb0df85b9a6911bab1860f7b2f307dd388600
Agustin <f.agustin@gmail.com>
2015-04-30 17:31:33 +0200

    move included

--- LBDyn/dynamics.py
+++ LBDyn/dynamics.py
@@ -1,8 +1,9 @@
 from numpy import *
+import numpy as np
 
 class LatticeBoltzmann:
 
-    def __init__(self, nx, ny):
+    def __init__(self, ny, nx):
 
         self.q = 9 # number of velocities
         self.d = 2 # dimension
@@ -28,32 +29,67 @@ class LatticeBoltzmann:
 
     def calc_eq_dens(self, rho, v):
 
-        # Density is in format (q, nx, ny) and ei is (q, dimension)
-        # v is the velocity vector, with dimension (dimension, nx, ny)
-        # rho is the density at each node, rho(nx, ny)
+        # Density is in format (q, ny, nx) and ei is (q, dimension)
+        # v is the velocity vector, with dimension (dimension, ny, nx)
+        # rho is the density at each node, rho(ny, nx)
+
+        dens_eq = np.ones ((self.q, self.ny, self.nx))
 
         # Make the dot product of ei and v.
-        # Dimensions -> edotv(q, nx, ny)
+        # Dimensions -> edotv(q, ny, nx)
         edotv = 3.0*dot(self.ei, v.transpose(1,0,2))
 
-        # Calculate the square velocity at each node, usqr(nx, ny)
+        # Calculate the square velocity at each node, usqr(ny, nx)
         vsqr = sum(v**2, axis = 0)
 
         # Calculate the equilibrium density
         for i in range(self.q):
-            dens_eq[i,:,:] = self.w[i]*rho*(1+edotv[i] + 
-                            0.5*edotv[i]**2.0 - 1.5*vsqr)
+            dens_eq[i,:,:] = self.w[i]*rho*(1+edotv[i] + 0.5*edotv[i]**2.0 - 1.5*vsqr)
 
         return dens_eq
 
     def calc_v_rho(self, dens):
 
-        # Densities are dens(q, nx, ny) and rho(nx, ny)
+        # Densities are dens(q, ny, nx) and rho(ny, nx)
         rho = sum(dens, axis = 0)
 
-        # ei(q, d) and dens(q, nx, ny). We need v(d, nx, ny).
+        # ei(q, d) and dens(q, ny, nx). We need v(d, ny, nx).
         v = dot(self.ei.transpose(), dens.transpose(1,0,2))/rho
 
         return v, rho
+
+    def move(self, dens):
+
+        dens_new[0,:,:] = dens[0,:,:]
+
+        dens_new[1,:,1:self.nx-1] = dens[1,:,0:self.nx-2]  
+        dens_new[1,:,0] = dens[1,:,self.nx-1]
+
+        dens_new[3,1:self.ny-1,:] = dens[3,0:self.ny-2,:]  
+        dens_new[3,0,:] = dens[3,self.ny-1,:]
+
+        dens_new[5,:,0:self.nx-2] = dens[5,:,1:self.nx-1]  
+        dens_new[5,:,self.nx-1] = dens[5,:,0]
+
+        dens_new[7,0:self.ny-2,:] = dens[7,1:self.ny-1,:]  
+        dens_new[7,self.ny-1,:] = dens[7,0,:]
+
+        dens_new[2,1:self.ny-1,1:self.nx-1] = dens[2,0:self.ny-2,0:self.nx-2]
+        dens_new[2,:,0] = dens[2,:,self.nx-1]  
+        dens_new[2,0,:] = dens[2,self.ny-1,:]
+
+        dens_new[4,1:self.ny-1,0:self.nx-2] = dens[4,0:self.ny-2,1:self.nx-1]
+        dens_new[4,:,self.nx-1] = dens[4,:,0]  
+        dens_new[4,0,:] = dens[4,self.ny-1,:]
+
+        dens_new[6,0:self.ny-2,0:self.nx-2] = dens[6,1:self.ny-1,1:self.nx-1]
+        dens_new[6,:,self.nx-1] = dens[6,:,0]  
+        dens_new[6,self.ny-1,:] = dens[6,0,:]
+
+        dens_new[8,0:self.ny-2,1:self.nx-1] = dens[8,1:self.ny-1,0:self.nx-2]
+        dens_new[8,:,0] = dens[8,:,self.nx-1]  
+        dens_new[8,self.ny-1,:] = dens[8,0,:]
+
+        return dens_new
 
 
--- main.py
+++ main.py
@@ -7,25 +7,32 @@ import numpy as np
 
 nx = 20
 ny = 7
-PGrad = 0.1/nx
+PGrad = 0.0001
 tau = 0.5
+omega = 1.0/tau
+t_max = 10
 
 #####################################
-model = LatticeBoltzmann(nx, ny)
+model = LatticeBoltzmann(ny, nx)
 # initial conditions
-v = np.zero(...)
-rho = np.ones(...)
-dens = ...
+v = np.zeros((model.d, ny, nx))
+rho = np.ones((ny,nx))
+dens_new = np.ones ((model.q, ny, nx))
+dens_old = np.ones ((model.q, ny, nx))
+dens_eq = np.ones ((model.q, ny, nx))
 
 # define boundaries (boolean array)
 
+wall = np.zeros((ny, nx))
+wall[0,:] = np.ones(nx)
+wall[ny-1,:] = np.ones(nx)
 model.wall = wall
 
 # evolute
 for t in np.linspace(0, t_max, 1):
-	dens_old = model.move(dens_new)
-	v, rho = model.calc_v_rho(v, rho)
-	v(0,:) = v(0,:) + 0.0001
+	#dens_old = model.move(dens_new)
+	v, rho = model.calc_v_rho(dens_old)
+	v[0,:] = v[0,:] + PGrad
 	dens_eq = model.calc_eq_dens(rho, v)
 	dens_new = (1-omega)*dens_old + omega*dens_eq
 

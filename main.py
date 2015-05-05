5ed491c6a88cc805bed980b08fd5a7eb19cb87da
epolimpio <epolimpio@gmail.com>
2015-05-05 16:43:01 +0200

    Almost there, we have to check if the exact profile is OK

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import matplotlib as matl
 import matplotlib.pyplot as plt
 
 # Parameters of the simulation
-nx = 100
-ny = 50
-PGrad = 1e-5
-tau = 0.9
+nx = 30
+ny = 11
+deltav = 1e-5
+tau = 1
 omega = 1.0/tau
 t_max = 10000
 
@@ -26,6 +26,13 @@ wall[0,:] = np.ones(nx).astype(bool)
 wall[ny-1,:] = np.ones(nx).astype(bool)
 fluid = np.logical_not(wall)
 
+# Exact Poiseuille flow
+visc = (tau-0.5)/3
+peak = deltav/tau/(2*visc)
+L = (ny-2)/2
+dist = np.linspace(-(L-0.5), (L-0.5), num = ny-2)
+exact = -peak*(dist**2-L**2)
+
 # Initiate graphics
 plt.ion()
 fig = plt.figure()
@@ -38,18 +45,20 @@ for t in np.arange(t_max):
     dens = model.bounce_back(dens, wall)
 
     v, rho = model.calc_v_rho(dens)
-    
+
     # Plot the velocity profile
     ax1.clear()
-    ax1.plot(np.arange(ny), np.mean(v[0,:,:], axis=1))
+    v_mean = np.mean(v[0,:,:], axis=1)
+    ax1.plot(dist, v_mean[1:ny-1])
+    ax1.plot(dist, exact)
     #ax1.pcolor(v[0,:,:])
     plt.draw()
     
     # Add velocity due to pressure gradient
-    v[0,fluid] = v[0,fluid] + PGrad
+    v[0,:,:] = v[0,:,:] + deltav
 
     # Calculate equilibrium and collide
     dens_eq = model.calc_eq_dens(rho, v)
-    dens[:,fluid] = (1-omega)*dens[:,fluid] + omega*dens_eq[:,fluid]
+    dens = (1-omega)*dens + omega*dens_eq
 
     print t

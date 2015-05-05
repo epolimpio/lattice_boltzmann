310636a3a2ef203c8803da88d0ec1a635b6c4d42
epolimpio <epolimpio@gmail.com>
2015-05-05 14:16:58 +0200

    With debugs and better parameters

--- LBDyn/dynamics.py
+++ LBDyn/dynamics.py
@@ -22,8 +22,9 @@ class LatticeBoltzmann:
         # Define the weights for the vectors
         self.w = 1.0/9.0 * np.ones(self.q)
         self.w[0] = 4.0/9.0
-        self.w[[2, 4, 6, 8]] = 10./36.0
+        self.w[[2, 4, 6, 8]] = 1.0/36.0
 
+        # Define the opposite vectors for bouncing back
         self.opposite = np.array([0,5,6,7,8,1,2,3,4])
 
         return True
@@ -34,7 +35,7 @@ class LatticeBoltzmann:
         # v is the velocity vector, with dimension (dimension, ny, nx)
         # rho is the density at each node, rho(ny, nx)
 
-        dens_eq = np.ones((self.q, self.ny, self.nx))
+        dens_eq = np.zeros((self.q, self.ny, self.nx))
 
         # Make the dot product of ei and v.
         # Dimensions -> edotv(q, ny, nx)
@@ -45,7 +46,7 @@ class LatticeBoltzmann:
 
         # Calculate the equilibrium density
         for i in range(self.q):
-            dens_eq[i,:,:] = self.w[i]*rho*(1+edotv[i, :, :] + 0.5*edotv[i, :, :]**2.0 - 1.5*vsqr)
+            dens_eq[i,:,:] = self.w[i]*rho*(1 + edotv[i,:,:] + 0.5*edotv[i,:,:]**2.0 - 1.5*vsqr)
 
         return dens_eq
 
@@ -61,48 +62,20 @@ class LatticeBoltzmann:
 
     def bounce_back(self, dens, wall):
         
-        for i in range(self.q):
-            dens[i, wall] = dens[self.opposite[i], wall]
-
-        return dens
-
-    def move(self, dens):
-
-        dens_new = dens
-
-        #dens_new[0,:,:] = dens[0,:,:]
-
-        # dens_new[1,:,1:self.nx-1] = dens[1,:,0:self.nx-2]  
-        # dens_new[1,:,0] = dens[1,:,self.nx-1]
-
-        # dens_new[3,1:self.ny-1,:] = dens[3,0:self.ny-2,:]  
-        # dens_new[3,0,:] = dens[3,self.ny-1,:]
-
-        # dens_new[5,:,0:self.nx-2] = dens[5,:,1:self.nx-1] 
-        # dens_new[5,:,self.nx-1] = dens[5,:,0]
-
-        # dens_new[7,0:self.ny-2,:] = dens[7,1:self.ny-1,:]  
-        # dens_new[7,self.ny-1,:] = dens[7,0,:]
-
-        # dens_new[2,1:self.ny-1,1:self.nx-1] = dens[2,0:self.ny-2,0:self.nx-2]
-        # dens_new[2,:,0] = dens[2,:,self.nx-1]  
-        # dens_new[2,0,:] = dens[2,self.ny-1,:]
-
-        # dens_new[4,1:self.ny-1,0:self.nx-2] = dens[4,0:self.ny-2,1:self.nx-1]
-        # dens_new[4,:,self.nx-1] = dens[4,:,0]  
-        # dens_new[4,0,:] = dens[4,self.ny-1,:]
-
-        # dens_new[6,0:self.ny-2,0:self.nx-2] = dens[6,1:self.ny-1,1:self.nx-1]
-        # dens_new[6,:,self.nx-1] = dens[6,:,0]  
-        # dens_new[6,self.ny-1,:] = dens[6,0,:]
-
-        # dens_new[8,0:self.ny-2,1:self.nx-1] = dens[8,1:self.ny-1,0:self.nx-2]
-        # dens_new[8,:,0] = dens[8,:,self.nx-1]  
-        # dens_new[8,self.ny-1,:] = dens[8,0,:]
+        dens_out = dens.copy()
 
         for i in range(self.q):
-            dens_new[i,:,:] = np.roll(np.roll(dens[i,:,:], self.ei[i,0], axis=0), self.ei[i,1], axis=1)
+           dens_out[i, wall] = dens[self.opposite[i], wall]
 
-        return dens_new
+        return dens_out
+
+    def stream(self, dens):
+
+        dens_out = dens.copy()
+
+        for i in range(self.q):
+           dens_out[i,:,:] = np.roll(np.roll(dens[i,:,:], self.ei[i,1], axis=0), self.ei[i,0], axis=1)
+
+        return dens_out
 
 
--- main.py
+++ main.py
@@ -8,41 +8,48 @@ import matplotlib.pyplot as plt
 # Parameters of the simulation
 nx = 100
 ny = 50
-PGrad = 0.00002
-tau = 0.5
+PGrad = 1e-5
+tau = 0.9
 omega = 1.0/tau
-t_max = 1000
+t_max = 10000
 
 #####################################
 model = LatticeBoltzmann(ny, nx)
-# initial conditions
+# Initial conditions
 v = np.zeros((model.d, ny, nx))
 rho = np.ones((ny,nx))
 dens = model.calc_eq_dens(rho, v)
 
-# define boundaries (boolean array)
-
+# Define boundaries (boolean array)
 wall = np.zeros((ny, nx)).astype(bool)
 wall[0,:] = np.ones(nx).astype(bool)
 wall[ny-1,:] = np.ones(nx).astype(bool)
 fluid = np.logical_not(wall)
-#model.wall = wall
 
 # Initiate graphics
 plt.ion()
 fig = plt.figure()
 ax1 = fig.add_subplot(1,1,1)
 
-# evolute
+# Dynamics
 for t in np.arange(t_max):
-	dens = model.move(dens)
-	dens = model.bounce_back(dens, wall)
-	v, rho = model.calc_v_rho(dens)
-	ax1.clear()
-	ax1.plot(np.arange(ny), np.mean(v[0,:,:], axis=1))
-	#ax1.pcolor(v[0,:,:])
-	plt.draw()
-	v[0,:,:] = v[0,:,:] + PGrad
-	dens_eq = model.calc_eq_dens(rho, v)
-	#dens[:,fluid] = (1-omega)*dens[:,fluid] + omega*dens_eq[:,fluid]
-	dens = (1-omega)*dens + omega*dens_eq
+    
+    dens = model.stream(dens)
+    dens = model.bounce_back(dens, wall)
+
+    v, rho = model.calc_v_rho(dens)
+    
+    # Plot the velocity profile
+    ax1.clear()
+    ax1.plot(np.arange(ny), np.mean(v[0,:,:], axis=1))
+    #ax1.pcolor(v[0,:,:])
+    plt.draw()
+    
+    # Add velocity due to pressure gradient
+    v[0,fluid] = v[0,fluid] + PGrad
+
+    # Calculate equilibrium and collide
+    dens_eq = model.calc_eq_dens(rho, v)
+    dens[:,fluid] = (1-omega)*dens[:,fluid] + omega*dens_eq[:,fluid]
+
+    print t
